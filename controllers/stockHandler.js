252abdf323231598ca31809027c181db47a1dfad
Cătălin Anghel-Ursu <cat.ang.dev@gmail.com>
2019-07-23 00:07:45 +0300

    Update stockHandler.js
    Very important to note is that the FCC's proposed google.finance API has been discontinued and as an alternative I've found alphavantage.co stock information API that's responsive and has a functional library on npm. I've written and separated the stock price checking and stock compare logic into 2 handlers: stockHandler.js and compareHandler.js. As in past projects, I found that using modularity in the code helps in reading and testing the code easier. The stockHandler.js handles the first form, where only one singular stock is requested and the compareHandler.js is handling the second form, where the 2 concurrent stocks are requested and compared.
    
    23.07_Update: Added a rule to catch the problem when the input code is not a valid stock code.

--- controllers/stockHandler.js
+++ controllers/stockHandler.js
@@ -10,68 +10,71 @@ function stockHandler (db, stock, like, ip, res) {
   //getting the stock data from the alphavantage API
   alpha.stocks.quote(stock, {"datatype": "json"})
     .then((data) => {
+    //console.log(data["Global Quote"]["01. symbol"]); 
     // Do what you want with the data
     const code = data["Global Quote"]["01. symbol"] //getting the stock code from the data obj
     const value = data["Global Quote"]["05. price"]; //getting the price value from the data obj
     
-    //stockEntry object
-    const stockEntry = {
-      stock   : code,
-      price   : value,
-      likes   : 0,
-      like    : like ? 1 : 0,
-      ips     : []
-    }
-    
-    like ? ++stockEntry.likes : stockEntry.likes; //adding the likes on a stock object, depending on the checkbox input status 
-    like ? stockEntry.ips.push(ip) : stockEntry.ips; //capturing the user's ip that issued the like - using it to ignore another like from that ip 
-    
-    db.collection("stocks").find({ stock : stock }, { projection : {_id: 0}})
-      .toArray((err, doc) => {
-      if (err) {
-        res.send(err);
-      } else {
-        if (doc[0] != undefined) { //the stock is already in the database --> proceed to update its props
-          //testing to see if the user has given a like, by looking for it's ip entry in the stock object.ips array
-          let ipExists = doc[0].ips.filter(el => el === ip); 
-          ipExists.length !== 0 ? ipExists = true : ipExists = false;
-          //console.log(ipExists);          
-          if (ipExists) { //user from this ip has already submitted a like
-            db.collection("stocks").updateOne(
-              { stock : stock },
-              { $set : { price : value }}
-            )
-          } else {
-            if (like) { //submit like and capture the user's ip in the ips array for later reference
+    if (code !== undefined) {
+      //stockEntry object
+      const stockEntry = {
+        stock   : code,
+        price   : value,
+        likes   : 0,
+        like    : like ? 1 : 0,
+        ips     : []
+      }
+      
+      like ? ++stockEntry.likes : stockEntry.likes; //adding the likes on a stock object, depending on the checkbox input status 
+      like ? stockEntry.ips.push(ip) : stockEntry.ips; //capturing the user's ip that issued the like - using it to ignore another like from that ip 
+      
+      db.collection("stocks").find({ stock : stock }, { projection : {_id: 0}})
+        .toArray((err, doc) => {
+        if (err) {
+          res.send(err);
+        } else {
+          if (doc[0] != undefined) { //the stock is already in the database --> proceed to update its props
+            //testing to see if the user has given a like, by looking for it's ip entry in the stock object.ips array
+            let ipExists = doc[0].ips.filter(el => el === ip); 
+            ipExists.length !== 0 ? ipExists = true : ipExists = false;
+            //console.log(ipExists); 
+            if (ipExists) { //user from this ip has already submitted a like
               db.collection("stocks").updateOne(
                 { stock : stock },
-                { $push : { ips : ip }, $set : { price : value, likes: ++doc[0].likes, like : like ? 1 : 0 }}
+                { $set : { price : value }}
               )
+            } else {
+              if (like) { //submit like and capture the user's ip in the ips array for later reference
+                db.collection("stocks").updateOne(
+                  { stock : stock },
+                  { $push : { ips : ip }, $set : { price : value, likes: ++doc[0].likes, like : like ? 1 : 0 }}
+                )
+              }
             }
-          }
-          err ? res.send(err) : res.send({
-            stockData : {
-              stock   : code,
-              price   : value,
-              likes   : doc[0].likes
-            }
-          });
-        } else { //the stock is not in the database so proceed to add a stockEntry object with it's props
-          db.collection("stocks")
-            .insertOne(stockEntry, (err, log) => {
             err ? res.send(err) : res.send({
               stockData : {
                 stock   : code,
                 price   : value,
-                likes   : stockEntry.likes
+                likes   : doc[0].likes
               }
             });
-            //console.log( stock.toUpperCase() + ' stock has been successfully submitted into the database.');
-            //console.log(log.ops);
-          });
-        }
-      } 
-    }); 
+          } else { //the stock is not in the database so proceed to add a stockEntry object with it's props
+            db.collection("stocks")
+              .insertOne(stockEntry, (err, log) => {
+              err ? res.send(err) : res.send({
+                stockData : {
+                  stock   : code,
+                  price   : value,
+                  likes   : stockEntry.likes
+                }
+              });
+              console.log( stock.toUpperCase() + ' stock has been successfully submitted into the database.');
+              console.log(log.ops);
+            });
+          }
+        } 
+      }); 
+    } else res.send(stock + ' stock not found! Please submit a valid stock code.');  
   })
     .catch((err) => {
     // Handle the error
